7b90ee48d23e149a790907c8193f134131fa981f
joespeaker <joebieker2486@outlook.com>
2022-04-04 02:10:06 -0500

    DARK MODE

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
 // Scripts
 // 
 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -32,3 +33,36 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+let darkmode = 0;
+function darkMode() {
+    let bg = document.getElementById("page-top");
+    let link = document.getElementsByTagName("a")
+    let text = document.getElementsByClassName("text-primary");
+    let nav = document.getElementsByClassName("nav-link");
+    if(darkmode == 0) {
+        bg.classList.add("document");
+        for(let i = 0; i < link.length; i++){
+            link[i].classList.add("darkmode");
+        }
+        for(let i = 0; i < nav.length; i++){
+            if(nav[i].getElementsByClassName("darkmode")){
+                nav[i].classList.remove("darkmode");
+            }
+        }
+        for(let i = 0; i < text.length; i++){
+            text[i].classList.add("darkmode");
+        }
+        darkmode++;
+    } else {
+        bg.classList.remove("document");
+        for(let i = 0; i < link.length; i++){
+            link[i].classList.remove("darkmode");
+        }
+        for(let i = 0; i < text.length; i++){
+            text[i].classList.remove("darkmode");
+        }
+        darkmode--;
+    }
+}
+
