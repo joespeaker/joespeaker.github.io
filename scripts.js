c6ab32cd0af4aab828e44c047a12678071b8074a
joespeaker <joebieker2486@outlook.com>
2022-04-08 10:50:02 -0500

    Improved Darkmode

--- scripts.js
+++ scripts.js
@@ -37,9 +37,13 @@ window.addEventListener('DOMContentLoaded', event => {
 let darkmode = 0;
 function darkMode() {
     let bg = document.getElementById("page-top");
-    let link = document.getElementsByTagName("a")
+    let link = document.getElementsByTagName("a");
     let text = document.getElementsByClassName("text-primary");
     let nav = document.getElementsByClassName("nav-link");
+    let p = document.getElementsByTagName("p");
+    let h2 = document.getElementsByTagName("h2");
+    let h3 = document.getElementsByTagName("h3");
+    let li = document.getElementsByTagName("li");
     if(darkmode == 0) {
         bg.classList.add("document");
         for(let i = 0; i < link.length; i++){
@@ -53,6 +57,19 @@ function darkMode() {
         for(let i = 0; i < text.length; i++){
             text[i].classList.add("darkmode");
         }
+        for(let i = 0; i < p.length; i++){
+            p[i].classList.add("darkermode");
+        }
+        for(let i = 0; i < h2.length; i++){
+            h2[i].classList.add("darkermode");
+        }
+        for(let i = 0; i < h3.length; i++){
+            h3[i].classList.add("darkermode");
+        }
+        for(let i = 0; i < li.length; i++){
+                li[i].classList.add("darkmode");
+        }
+        
         darkmode++;
     } else {
         bg.classList.remove("document");
@@ -61,6 +78,20 @@ function darkMode() {
         }
         for(let i = 0; i < text.length; i++){
             text[i].classList.remove("darkmode");
+        }
+        for(let i = 0; i < p.length; i++){
+            p[i].classList.remove("darkermode");
+        }
+        for(let i = 0; i < h2.length; i++){
+            h2[i].classList.remove("darkermode");
+        }
+        for(let i = 0; i < h3.length; i++){
+            h3[i].classList.remove("darkermode");
+        }
+        for(let i = 0; i < h3.length; i++){
+            h3[i].classList.remove("darkermode");
+        }for(let i = 0; i < li.length; i++){
+            li[i].classList.remove("darkmode");
         }
         darkmode--;
     }
